e12ed8571770a0cd919625b9080d0e2b80b71f59
pedrocesarolvc <cesar.pedrinho10@gmail.com>
2025-05-21 19:29:53 -0300

    Add files via upload

---
+++ linux/sistema_pagamentos.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Arquivo de pedidos
+DB="pedidos.txt"
+
+# Garante que o arquivo existe
+touch "$DB"
+# Fazer uma função para pedir o produtoe só depois registrar o status do pagamento
+registrar_pagamento() {
+    read -p "Número do pedido: " pedido
+    data=$(date)
+    echo "$pedido|PAGO|$data|AGUARDANDO_ENTREGA" >> "$DB"
+    echo "Nota fiscal emitida para o pedido $pedido."
+    echo "Status: PAGO"
+    echo "Data: $data"
+}
+
+iniciar_entrega() {
+    read -p "Número do pedido: " pedido
+
+    if grep -q "^$pedido|" "$DB"; then
+        sed -i "s/^$pedido|PAGO|.*|AGUARDANDO_ENTREGA/$pedido|PAGO|$(date)|EM_ENTREGA/" "$DB"
+        read -p "Tempo de entrega estimado (em segundos): " tempo
+
+        echo "Pedido $pedido saiu para entrega!"
+        while (( "$tempo" > 0 )) ; do
+            echo "Tempo restante: $tempo segundos"
+            sleep 1
+            ((tempo--))
+        done
+
+        sed -i "s/^$pedido|PAGO|.*|EM_ENTREGA/$pedido|PAGO|$(date)|ENTREGUE/" "$DB"
+        echo "Pedido $pedido ENTREGUE!"
+    else
+        echo "Pedido não encontrado!"
+    fi
+}
+
+ver_status() {
+    echo "=== STATUS DOS PEDIDOS ==="
+
+    if [ -s "$DB" ]; then
+        column -t -s '|' "$DB"
+    else
+        echo "Nenhum pedido registrado."
+    fi
+}
+
+while true; do
+    echo ""
+    echo "==== MENU DE OPÇÕES ===="
+    echo "1. Registrar pagamento"
+    echo "2. Iniciar entrega"
+    echo "3. Ver status de pedidos"
+    echo "4. Sair"
+    read -p "Escolha uma opção: " opcao
+
+    case "$opcao" in
+        1)
+            registrar_pagamento
+            ;;
+        2)
+            iniciar_entrega
+            ;;
+        3)
+            ver_status
+            ;;
+        4)
+            echo "Saindo..."
+            break
+            ;;
+        *)
+            echo "Opção inválida!"
+            ;;
+    esac
+done
