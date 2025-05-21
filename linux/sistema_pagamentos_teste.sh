780dc24a7b1df8f5663195cbafd2816c244ff245
pedrocesarolvc <pedrocolvcouto@gmail.com>
2025-05-21 20:01:20 -0300

    criação e alteração dos primeiros passos das funções

--- linux/sistema_pagamentos_teste.sh
+++ linux/sistema_pagamentos_teste.sh
@@ -5,6 +5,43 @@ DB="pedidos.txt"
 # Garante que o arquivo existe
 touch "$DB"
 # Fazer uma função para pedir o produtoe só depois registrar o status do pagamento
+
+registrar_produto() {
+    echo "Selecione um Produto: "
+    echo "1. Carregador"
+    echo "2. Cabo"
+    echo "3. Capinha"
+    echo "4. Película"
+    echo "5. Fone"
+    read -p "Escolha um item: " item
+}
+while true do
+    case "$item" in
+            1)
+                echo -p "Carregador Escolhido" produto
+                break
+                ;;
+            2)
+                echo -p "Cabo Escolhido" produto
+                break
+                ;;
+            3)
+                echo -p "Capinha Escolhido" produto
+                break
+                ;;
+            4)
+                echo -p "Película Escolhido" produto
+                break
+                ;;
+            5)
+                echo -p "Fone Escolhido" produto
+                break
+                ;;
+            *)
+                echo "Opção inválida!"
+                ;;
+        esac
+done
 registrar_pagamento() {
     read -p "Número do pedido: " pedido
     data=$(date)
@@ -48,23 +85,27 @@ ver_status() {
 while true; do
     echo ""
     echo "==== MENU DE OPÇÕES ===="
-    echo "1. Registrar pagamento"
-    echo "2. Iniciar entrega"
-    echo "3. Ver status de pedidos"
-    echo "4. Sair"
+    echo "1. Registrar produto"
+    echo "2. Registrar pagamento"
+    echo "3. Iniciar entrega"
+    echo "4. Ver status de pedidos"
+    echo "5. Sair"
     read -p "Escolha uma opção: " opcao
 
     case "$opcao" in
         1)
-            registrar_pagamento
+            registrar_produto
             ;;
         2)
-            iniciar_entrega
+            registrar_pagamento
             ;;
         3)
-            ver_status
+            iniciar_entrega
             ;;
         4)
+            ver_status
+            ;;
+        5)
             echo "Saindo..."
             break
             ;;
